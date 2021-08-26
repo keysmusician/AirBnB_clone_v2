cbea541a6ed7e657a862203dd95e79017586dfdf
keysmusician <justin.masayda@gmail.com>
2021-08-26 17:04:04 +0000

    Test using sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,27 +6,13 @@ apt update
 apt -y install nginx
 
 # Create static content
-mkdir -p /data/web_static/releases/
-mkdir /data/web_static/shared/
-mkdir /data/web_static/releases/test/
+mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
 echo "Air BnB Clone 0x03. Web static test" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/current /data/web_static/releases/test/
 chown -R ubuntu:ubuntu /data/
 
 # Configure Nginx
-cat > /etc/nginx/sites-available/default <<"EOF"
-server {
-    listen 80;
-    listen [::]:80 default_server;
-    root /data/web_static;
-    index index.html;
-    add_header X-Served-By $HOSTNAME;
-
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
-}
-EOF
+sed -i '37i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 # Launch nginx
 service nginx restart

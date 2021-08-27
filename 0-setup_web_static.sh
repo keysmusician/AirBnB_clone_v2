d96785e07cb3d8b78e393c4da67d70610b63e33c
keysmusician <justin.masayda@gmail.com>
2021-08-27 02:21:52 +0000

    Update task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,19 +7,15 @@ apt -y install nginx
 
 # Create static content
 mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-echo "Air BnB Clone 0x03. Web static test" > /data/web_static/releases/test/index.html
+echo "Holberton School Air BnB Clone 0x03. Web static test" > \
+/data/web_static/releases/test/index.html
 ln -sf /data/web_static/current /data/web_static/releases/test/
 chown -R ubuntu:ubuntu /data/
 
 # Configure Nginx
 cat > /etc/nginx/sites-available/default <<"EOF"
 server {
-    listen 80;
-    listen [::]:80 default_server;
-    root /data/web_static;
-    index index.html;
     add_header X-Served-By $HOSTNAME;
-
     location /hbnb_static {
         alias /data/web_static/current/;
     }

e61f08d3ed66be28e91dac556987195e4afeffd6
keysmusician <justin.masayda@gmail.com>
2021-09-12 19:52:46 +0000

    Add task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application."""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Display a hello message."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display 'HBNB'."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Display varaible text."""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """Display varaible text."""
+    return 'Python ' + text.replace('_', ' ')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Display variable text if it is an integer."""
+    return '{} is a number'.format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

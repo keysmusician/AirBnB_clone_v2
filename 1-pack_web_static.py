c228b26940bb49ceabf139ce2cde5216ce662824
keysmusician <justin.masayda@gmail.com>
2021-09-12 22:01:24 +0000

    Make all .py executable

---
+++ 1-pack_web_static.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Generates a .tgz archive from the contents of the "web_static" folder of my AirBnB Clone repo.
+
+"""
+from fabric.api import run
+
+def do_pack():
+    """Generate .tgz archive."""
+    run('echo "test"')

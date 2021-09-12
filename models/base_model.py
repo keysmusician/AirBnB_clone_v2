b8a2cdc15b7376c4f70a223b09549a81a223b8ab
keysmusician <justin.masayda@gmail.com>
2021-09-12 22:00:21 +0000

    Fix BaseModel instantiation

--- models/base_model.py
+++ models/base_model.py
@@ -13,25 +13,28 @@ Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """A base class for all HBnB models."""
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instatntiate a new model."""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        """Instantiate a new model."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        # Set each keyword argument as an instance attribute
+        for k, v in kwargs.items():
+            # Do not set `__class__`, since it is set automatically
+            if k == '__class__':
+                continue
+
+            # Convert datetime strings to datetime objects
+            if k in ('updated_at', 'created_at'):
+                v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+
+            setattr(self, k, v)
 
     def __str__(self):
         """Return a string representation of the instance."""

2ac8174e04e8835c7bee7c97694bc519d80191f9
keysmusician <justin.masayda@gmail.com>
2021-09-15 23:33:40 +0000

    Update module docstring

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This script starts a Flask web application."""
+"""Starts a Flask web application."""
 from flask import Flask, render_template
 from models import storage
 from models.state import State

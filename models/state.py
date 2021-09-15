046e8613145f282f004e1579fad8a177fe1a4728
keysmusician <justin.masayda@gmail.com>
2021-09-15 15:46:16 +0000

    Add import statement

--- models/state.py
+++ models/state.py
@@ -14,11 +14,7 @@ class State(BaseModel, Base):
 
     @property
     def cities(self):
+        from models import storage
         """Return the list of cities matching this state."""
         return [city for city in storage.all(City).values()
                 if city.state_id == self.id]
-
-if __name__ == '__main__':
-    # Storage is defined prior to importing these modules, but needs to be
-    # imported here if running stand-alone.
-    from models import storage

70b62d491114295278fd934a89de1cb3c8fd6429
Justin Masayda <justin.masayda@gmail.com>
2021-11-29 12:11:26 -0500

    Correct displayed text

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_world():
     """Display a Hello World"""
-    return 'Hello, HBNB!'
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
